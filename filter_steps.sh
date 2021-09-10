169cdac576f9e839e36ed5717fed6e8354b44aaf
bmichanderson <banderson2914@gmail.com>
2021-09-10 17:20:06 +0800

    added file removal and conditional command exec

--- filter_steps.sh
+++ filter_steps.sh
@@ -6,6 +6,7 @@
 # Description: run a successive series of filtering steps on VCF files
 # NOTE: based on O'Leary et al. 2018 DOI: 10.1111/mec.14792
 # NOTE: it requires R scripts, as well as vcftools in the PATH
+# WARNING: it removes some files it creates, so best run when the directory does not have anything named with out_prefix
 ###############
 
 
@@ -20,7 +21,7 @@ max_indv_miss="0.2"        # maximum proportion missing data for a sample
 minDP="6"                  # minimum depth for a genotype
 min_meanDP="10"            # minimum mean depth for a locus (across *all* individuals)
 minor_count="3"            # alleles must occur >= this many times to be kept
-iter_snp=(0.2 0.4 0.6 0.8)     # the progressive filters (need to be the same length)
+iter_snp=(0.2 0.4 0.6 0.8)     # the progressive filters (needs to be the same length as iter_indv)
 iter_indv=(0.9 0.7 0.5 0.3)
 
 
@@ -58,13 +59,15 @@ filter_depth()
 {
     vcftools --vcf $1 --out "$2"mDP --minDP $3 --recode --recode-INFO-all && \
     calc_stats "$2"mDP.recode.vcf "$2"mDP && \
-    Rscript $plot_script -p "$2"mDP |& tee -a filters_log.txt && \
+    Rscript $plot_script -p "$2"mDP |& tee -a "$out_prefix"_log.txt && \
     vcftools --vcf "$2"mDP.recode.vcf --out "$2"mDPmmDP --min-meanDP $4 --recode --recode-INFO-all && \
+    rm "$2"mDP.* && \
     calc_stats "$2"mDPmmDP.recode.vcf "$2"mDPmmDP && \
-    Rscript $plot_script -p "$2"mDPmmDP |& tee -a filters_log.txt && \
+    Rscript $plot_script -p "$2"mDPmmDP |& tee -a "$out_prefix"_log.txt && \
     vcftools --vcf "$2"mDPmmDP.recode.vcf --out "$2"mDPmmDPmac --mac $5 --recode --recode-INFO-all && \
+    rm "$2"mDPmmDP.* && \
     calc_stats "$2"mDPmmDPmac.recode.vcf "$2"mDPmmDPmac && \
-    Rscript $plot_script -p "$2"mDPmmDPmac |& tee -a filters_log.txt
+    Rscript $plot_script -p "$2"mDPmmDPmac |& tee -a "$out_prefix"_log.txt
 }
 
 
@@ -131,41 +134,51 @@ fi
 ## Run the filtering
 # calculate stats for raw
 if [ "$run_setting" == "1" ]; then
-    calc_stats $vcf_file $out_prefix
-    Rscript $plot_script -p $out_prefix |& tee -a filters_log.txt
+    calc_stats $vcf_file $out_prefix && \
+    Rscript $plot_script -p $out_prefix |& tee -a "$out_prefix"_log.txt && \
+    rm "$out_prefix".*
 fi
 # run further filtering if requested
 if [ "$run_setting" == "2" ]; then
     # F1 straight to strict filter, SNP first
-    filter_snp $vcf_file "$out_prefix"_snp $min_cover
-    filter_indv "$out_prefix"_snp.recode.vcf "$out_prefix"_snp_indv $max_indv_miss
-    calc_stats "$out_prefix"_snp_indv.recode.vcf "$out_prefix"_F1
-    Rscript $plot_script -p "$out_prefix"_F1 |& tee -a filters_log.txt
+    filter_snp $vcf_file "$out_prefix"_snp $min_cover && \
+    filter_indv "$out_prefix"_snp.recode.vcf "$out_prefix"_snp_indv $max_indv_miss && \
+    calc_stats "$out_prefix"_snp_indv.recode.vcf "$out_prefix"_F1 && \
+    Rscript $plot_script -p "$out_prefix"_F1 |& tee -a "$out_prefix"_log.txt && \
+    mv "$out_prefix"_snp_indv.recode.vcf "$out_prefix"_F1_out.vcf && \
+    rm "$out_prefix"_snp* "$out_prefix"_F1.*
 
     # F2 straight to strict filter, indv first
-    filter_indv $vcf_file "$out_prefix"_indv $max_indv_miss
-    filter_snp "$out_prefix"_indv.recode.vcf "$out_prefix"_indv_snp $min_cover
-    calc_stats "$out_prefix"_indv_snp.recode.vcf "$out_prefix"_F2
-    Rscript $plot_script -p "$out_prefix"_F2 |& tee -a filters_log.txt
+    filter_indv $vcf_file "$out_prefix"_indv $max_indv_miss && \
+    filter_snp "$out_prefix"_indv.recode.vcf "$out_prefix"_indv_snp $min_cover && \
+    calc_stats "$out_prefix"_indv_snp.recode.vcf "$out_prefix"_F2 && \
+    Rscript $plot_script -p "$out_prefix"_F2 |& tee -a "$out_prefix"_log.txt && \
+    mv "$out_prefix"_indv_snp.recode.vcf "$out_prefix"_F2_out.vcf && \
+    rm "$out_prefix"_indv* "$out_prefix"_F2.*
 
     # F3 depth first, then to strict filter, SNP first
-    filter_depth $vcf_file "$out_prefix" $minDP $min_meanDP $minor_count
-    filter_snp "$out_prefix"mDPmmDPmac.recode.vcf "$out_prefix"_depth_snp $min_cover
-    filter_indv "$out_prefix"_depth_snp.recode.vcf "$out_prefix"_depth_snp_indv $max_indv_miss
-    calc_stats "$out_prefix"_depth_snp_indv.recode.vcf "$out_prefix"_F3
-    Rscript $plot_script -p "$out_prefix"_F3 |& tee -a filters_log.txt
+    filter_depth $vcf_file "$out_prefix" $minDP $min_meanDP $minor_count && \
+    filter_snp "$out_prefix"mDPmmDPmac.recode.vcf "$out_prefix"_depth_snp $min_cover && \
+    filter_indv "$out_prefix"_depth_snp.recode.vcf "$out_prefix"_depth_snp_indv $max_indv_miss && \
+    calc_stats "$out_prefix"_depth_snp_indv.recode.vcf "$out_prefix"_F3 && \
+    Rscript $plot_script -p "$out_prefix"_F3 |& tee -a "$out_prefix"_log.txt && \
+    mv "$out_prefix"_depth_snp_indv.recode.vcf "$out_prefix"_F3_out.vcf && \
+    rm "$out_prefix"_depth_snp* "$out_prefix"_F3.*
 
-    # F4 depth first (already done), then to iterative filters, then strict, indv first
-    cp "$out_prefix"mDPmmDPmac.recode.vcf temp_in.vcf
+    # F4 depth first (already done), then to iterative filters, then to strict filter, indv first
+    mv "$out_prefix"mDPmmDPmac.recode.vcf temp_in.vcf && \
+    rm "$out_prefix"mDPmmDPmac.*
     for iter in $(seq 1 ${#iter_snp[@]})
     do
-        filter_snp temp_in.vcf "$out_prefix"_"$iter"_snp ${iter_snp[iter - 1]}
-        filter_indv "$out_prefix"_"$iter"_snp.recode.vcf "$out_prefix"_"$iter"_snp_indv ${iter_indv[iter - 1]}
-        cp "$out_prefix"_"$iter"_snp_indv.recode.vcf temp_in.vcf
+        filter_snp temp_in.vcf "$out_prefix"_"$iter"_snp ${iter_snp[iter - 1]} && \
+        filter_indv "$out_prefix"_"$iter"_snp.recode.vcf "$out_prefix"_"$iter"_snp_indv ${iter_indv[iter - 1]} && \
+        mv "$out_prefix"_"$iter"_snp_indv.recode.vcf temp_in.vcf
     done
-    filter_indv temp_in.vcf "$out_prefix"_iter_indv $max_indv_miss
-    filter_snp "$out_prefix"_iter_indv.recode.vcf "$out_prefix"_iter_indv_snp $min_cover
-    calc_stats "$out_prefix"_iter_indv_snp.recode.vcf "$out_prefix"_F4
-    Rscript $plot_script -p "$out_prefix"_F4 |& tee -a filters_log.txt
-    rm temp_in.vcf
+    rm "$out_prefix"_*_snp*
+    filter_indv temp_in.vcf "$out_prefix"_iter_indv $max_indv_miss && \
+    filter_snp "$out_prefix"_iter_indv.recode.vcf "$out_prefix"_iter_indv_snp $min_cover && \
+    calc_stats "$out_prefix"_iter_indv_snp.recode.vcf "$out_prefix"_F4 && \
+    Rscript $plot_script -p "$out_prefix"_F4 |& tee -a "$out_prefix"_log.txt && \
+    mv "$out_prefix"_iter_indv_snp.recode.vcf "$out_prefix"_F4_out.vcf && \
+    rm "$out_prefix"_iter_indv* "$out_prefix"_F4.* temp_in.vcf
 fi
