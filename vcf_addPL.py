10bdd5fa9bdfac988647084dd12444fd49abb160
bmichanderson <banderson2914@gmail.com>
2021-10-27 19:14:49 +0800

    created scripts to add PL fields and convert to Beagle format

---
+++ vcf_addPL.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+##########################
+# Author: B. Anderson
+# Date: Oct 2021
+# Description: convert a VCF file without PL fields into one with (for Tiger to update with values)
+##########################
+
+
+import sys
+import argparse
+
+
+# instantiate the parser
+parser = argparse.ArgumentParser(description = 'A script to convert a VCF file without PL fields to one with')
+
+
+# add arguments to parse
+parser.add_argument('vcf_file', type = str, help = 'The VCF file to convert')
+
+
+# parse the command line
+if len(sys.argv[1:]) == 0:		# if there are no arguments
+	parser.print_help(sys.stderr)
+	sys.exit(1)
+
+args = parser.parse_args()
+
+vcf_file = args.vcf_file
+
+if not vcf_file:
+	parser.print_help(sys.stderr)
+	sys.exit(1)
+
+
+# process the VCF
+with open(vcf_file, 'r') as vcf, open('modPL_' + vcf_file, 'w') as outfile:
+	for line in vcf:
+		if line.startswith('#'):        # a header INFO line
+			if line.startswith('#CHROM'):		# the line with sample names
+				outfile.write('##FORMAT=<ID=PL,Number=G,Type=Float,Description=\"Phred-scaled genotype likelihoods\">\n')
+				outfile.write(line)
+			else:
+				outfile.write(line)
+		else:
+			pieces = line.rstrip().split()
+			line_out = pieces[:8]
+			line_out.append(pieces[8] + ':PL')
+			for sample_piece in pieces[9:]:
+				line_out.append(sample_piece + ':0.0,0.0,0.0')
+			outfile.write('\t'.join(line_out) + '\n')
+
+# report completion
+print('Done')
---
+++ vcf_to_beagle.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+
+##########################
+# Author: B. Anderson, with inspiration for some formulas from https://github.com/darencard/RADpipe/blob/master/genotype_from_VCF.py
+# Date: Oct 2021
+# Description: convert a VCF file with PL fields into Beagle format
+##########################
+
+
+import sys
+import argparse
+
+
+# instantiate the parser
+parser = argparse.ArgumentParser(description = 'A script to convert a VCF file with PL fields to a Beagle format')
+
+
+# add arguments to parse
+parser.add_argument('vcf_file', type = str, help = 'The VCF file to convert')
+
+
+# parse the command line
+if len(sys.argv[1:]) == 0:		# if there are no arguments
+	parser.print_help(sys.stderr)
+	sys.exit(1)
+
+args = parser.parse_args()
+
+vcf_file = args.vcf_file
+
+if not vcf_file:
+	parser.print_help(sys.stderr)
+	sys.exit(1)
+
+
+# process the VCF to grab sample labels and alleles and PL values for each SNP locus
+with open(vcf_file, 'r') as vcf:
+	phredls = []
+	snp_names = []
+	alleles = []
+	count_snps = 0
+	for line in vcf:
+		if line.startswith('#'):        # a header INFO line
+			if line.startswith('#CHROM'):		# the line with sample names
+				sample_labels = line.rstrip().split()[9:]
+		else:
+			pieces = line.rstrip().split()
+			snp_name = pieces[2]
+			allele_ref = pieces[3]
+			allele_alt = pieces[4]
+			pls = []
+			calls = pieces[9:]
+			for call in calls:
+				'''	The format expected is GT:DP:CATG:PL, e.g. 0/0:85:0,85,0,0:0,25.51,13.2
+					We want to grab the Phred-scaled genotype likelihood field (PL) 0,25.51,13.2
+					then split that into the three values as a list
+				'''
+				pls.append(call.split(':')[-1].split(','))
+			phredls.append(pls)
+			alleles.append((allele_ref, allele_alt))
+			snp_names.append(snp_name)
+			count_snps = count_snps + 1
+	print('Read in a VCF file with ' + str(len(sample_labels)) + ' samples and ' + str(count_snps) + ' SNP loci')
+
+
+# create the output lines to write to the Beagle file
+header_line = ('Marker\tRef\tAlt\t' + ''.join(3 * (sample + '\t') for sample in sample_labels))
+lines_out = []
+for snp_index, phredl in enumerate(phredls):
+	line_out = (str(snp_names[snp_index]) + '\t' + '\t'.join(alleles[snp_index]) + '\t')
+	val_list = []
+	for sample_index, sample_label in enumerate(sample_labels):
+		this_pl = phredl[sample_index]
+		if this_pl == ['.']:		# missing data
+			norm_00 = 0.0
+			norm_01 = 0.0
+			norm_11 = 0.0
+		else:
+			like_00 = float(10 ** (float(this_pl[0]) / -10))
+			like_01 = float(10 ** (float(this_pl[1]) / -10))
+			like_11 = float(10 ** (float(this_pl[2]) / -10))
+			sum_likes = float(like_00 + like_01 + like_11)
+			norm_00 = float(like_00 / sum_likes)
+			norm_01 = float(like_01 / sum_likes)
+			norm_11 = float(like_11 / sum_likes)
+			norms = [norm_00, norm_01, norm_11]
+		val_list.append('\t'.join('{0:.4f}'.format(item) for item in norms))
+	line_out = line_out + '\t'.join(val_list)
+	lines_out.append(line_out)
+
+
+# create and write to the output file
+with open(vcf_file + '.beagle', 'w') as outfile:
+	outfile.write(header_line + '\n')
+	for line_out in lines_out:
+		outfile.write(line_out + '\n')
+
+
+# report completion
+print('Done')
