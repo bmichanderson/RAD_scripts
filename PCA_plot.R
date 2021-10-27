030b6af762949717081a1bb1188c2f5cf20f1314
bmichanderson <banderson2914@gmail.com>
2021-10-27 19:13:36 +0800

    added support for covariance matrix

--- PCA_plot.R
+++ PCA_plot.R
@@ -2,7 +2,7 @@
 ##########
 # Author: Ben Anderson
 # Date: Oct 2021
-# Description: run a PCA on a VCF file and plot
+# Description: run a PCA on a VCF file and plot; now can alternatively plot from a covariance matrix from PCAngsd
 ##########
 
 
@@ -37,25 +37,26 @@ draw_colours <- c(
 # a helper function for errors or no args
 help <- function(help_message) {
 	if (missing(help_message)) {
-		cat("A script to run a PCA and plot it using an input VCF file\n",
+		cat("A script to run a PCA and plot it using an input VCF file or a covariance matrix file\n",
 			"It is assumed that the SNPs in the VCF are already one per locus and biallelic\n")
-		cat("Usage: Rscript PCA_plot.R -o output -s sample_file -v vcf_file\n")
+		cat("Usage: Rscript PCA_plot.R -o output -s sample_file [-v vcf_file, -c covmat_file]\n")
 		cat("Options:\n")
 		cat("\t-o\tThe output file name prefix [default output]\n")
 		cat("\t-s\tThe file with tab-delimited sample IDs and pops, one per line, for labelling/colouring the plot\n")
-		cat("\t-v\tThe VCF file to be analysed\n")
+		cat("\t-v\tThe VCF file to be analysed (takes priority)\n")
+		cat("\t-c\tThe covariance matrix file (not used if VCF present)\n")
 	} else {
 		cat(help_message)
 	}
 }
 
 # a function to plot a PCA
-plot_pca <- function(pca_scores, pca_var, x = 1, y = 2, pca_colour, legend, legend_colour, num_snps) {
+plot_pca <- function(pca_scores, pca_var, x = 1, y = 2, pca_colour, legend, legend_colour, method) {
 	par(mar = c(5.1, 4.1, 4.1, 6.1))
 	plot(pca_scores[, x], pca_scores[, y],
 		xlab = paste0("PC", x, " (", pca_var[x], "%)"),
 		ylab = paste0("PC", y, " (", pca_var[y], "%)"),
-		main = paste0("Principal Component Analysis\n(PC", y, " vs PC", x, ") using ", num_snps, " SNPs"),
+		main = paste0("Principal Component Analysis\n(PC", y, " vs PC", x, ") using ", method),
 		col = adjustcolor(pca_colour, alpha.f = 0.8),
 		pch = 19,
 		panel.first = {
@@ -82,6 +83,7 @@ if (length(args) == 0) {
 	output <- "output"
 	samples_present <- FALSE
 	vcf_present <- FALSE
+	covmat_present <- FALSE
 	for (index in seq_len(length(args))) {
 		if (args[index] == "-o") {
 			output <- args[index + 1]
@@ -91,6 +93,9 @@ if (length(args) == 0) {
 		} else if (args[index] == "-v") {
 			vcf_present <- TRUE
 			vcf_file <- args[index + 1]
+		} else if (args[index] == "-c") {
+			covmat_present <- TRUE
+			covmat_file <- args[index + 1]
 		} else {
 			catch_args[i] <- args[index]
 			i <- i + 1
@@ -100,37 +105,70 @@ if (length(args) == 0) {
 if (! samples_present) {
 	stop(help("Missing argument for sample/pops file!\n"), call. = FALSE)
 }
-if (! vcf_present) {
-	stop(help("Missing argument for vcf file!\n"), call. = FALSE)
+if (all(! vcf_present, ! covmat_present)) {
+	stop(help("Missing argument for vcf file or covariance matrix file!\n"), call. = FALSE)
 }
 
 
 # read in the input files
 sample_table <- read.table(samples_file, sep = "\t", header = FALSE)
-vcf <- read.vcfR(vcf_file, verbose = FALSE)
-cat("Read in a VCF with", ncol(vcf@gt) - 1, "samples,",
-	length(unique(vcf@fix[, 1])), "loci and", nrow(vcf@fix), "SNPs\n")
-
-
-# convert to genlight
-genl <- vcfR2genlight(vcf)
-
-
-# check that the samples match
-if (length(indNames(genl)) != nrow(sample_table)) {
-	stop(help("Number of samples in the VCF does not match number in the pops file\n"), call. = FALSE)
+if (vcf_present) {
+	vcf <- read.vcfR(vcf_file, verbose = FALSE)
+	cat("Read in a VCF with", ncol(vcf@gt) - 1, "samples,",
+		length(unique(vcf@fix[, 1])), "loci and", nrow(vcf@fix), "SNPs\n")
+} else if (covmat_present) {
+	covmat <- as.matrix(read.table(covmat_file))
 }
 
-for (sample in sample_table$V1) {
-	if (! sample %in% indNames(genl)) {
-		stop(help("Sample", sample, "not found in VCF\n"), call. = FALSE)
+
+# Processing
+if (vcf_present) {
+	# convert to genlight
+	genl <- vcfR2genlight(vcf)
+
+	# check that the samples match
+	if (length(indNames(genl)) != nrow(sample_table)) {
+		stop(help("Number of samples in the VCF does not match number in the pops file\n"), call. = FALSE)
 	}
+
+	for (sample in sample_table$V1) {
+		if (! sample %in% indNames(genl)) {
+			stop(help("Sample", sample, "not found in VCF\n"), call. = FALSE)
+		}
+	}
+
+	# assign population labels to the genlight object
+	populations <- sample_table$V2[match(indNames(genl), sample_table$V1)]
+	pop(genl) <- populations
+
+	# create PCA
+	pca <- glPca(genl, nf = 5, parallel = TRUE)
+
+	# calculate percent variation explained for each PC
+	pca_var <- round(100 * pca$eig / sum(pca$eig), digits = 1)
+
+	# set the method used text for plotting
+	method <- paste0("glPca on ", str(nLoc(genl)), " SNPs")
+
+	# set what the pca_scores comprise
+	pca_scores <- pca$scores
+
+} else if (covmat_present) {
+	# capture pop labels
+	populations <- sample_table$V2
+
+	# convert to eigen
+	eig <- eigen(covmat)
+
+	# calculate percent variation explained for the eigenvalues
+	pca_var <- round(100 * eig$values / sum(eig$values), digits = 1)
+
+	# set the method used text for plotting
+	method <- paste0("a covariance matrix based on allele frequencies")
+
+	# set what the pca_scores comprise
+	pca_scores <- eig$vectors
 }
-
-
-# assign population labels to the genlight object
-populations <- sample_table$V2[match(indNames(genl), sample_table$V1)]
-pop(genl) <- populations
 
 
 # assign colours to the individuals/populations
@@ -146,46 +184,40 @@ for (pop in unique(populations)) {
 	ind <- ind + 1
 }
 
-# create PCA
-pca <- glPca(genl, nf = 5, parallel = TRUE)
-
-
-# calculate percent variation explained for each PC
-pca_var <- round(100 * pca$eig / sum(pca$eig), digits = 1)
-
 
 # Plot PCAs
 pdf(paste0(output, "_pca.pdf"), paper = "A4")
-plot_pca(pca$scores, pca_var, x = 1, y = 2, pca_colour = indiv_colours,
-		legend = unique(populations), legend_colour = pop_colours, num_snps = nLoc(genl))
-plot_pca(pca$scores, pca_var, x = 1, y = 3, pca_colour = indiv_colours,
-		legend = unique(populations), legend_colour = pop_colours, num_snps = nLoc(genl))
-plot_pca(pca$scores, pca_var, x = 1, y = 4, pca_colour = indiv_colours,
-		legend = unique(populations), legend_colour = pop_colours, num_snps = nLoc(genl))
-plot_pca(pca$scores, pca_var, x = 2, y = 3, pca_colour = indiv_colours,
-		legend = unique(populations), legend_colour = pop_colours, num_snps = nLoc(genl))
-plot_pca(pca$scores, pca_var, x = 3, y = 4, pca_colour = indiv_colours,
-		legend = unique(populations), legend_colour = pop_colours, num_snps = nLoc(genl))
+plot_pca(pca_scores, pca_var, x = 1, y = 2, pca_colour = indiv_colours,
+		legend = unique(populations), legend_colour = pop_colours, method = method)
+plot_pca(pca_scores, pca_var, x = 1, y = 3, pca_colour = indiv_colours,
+		legend = unique(populations), legend_colour = pop_colours, method = method)
+plot_pca(pca_scores, pca_var, x = 1, y = 4, pca_colour = indiv_colours,
+		legend = unique(populations), legend_colour = pop_colours, method = method)
+plot_pca(pca_scores, pca_var, x = 2, y = 3, pca_colour = indiv_colours,
+		legend = unique(populations), legend_colour = pop_colours, method = method)
+plot_pca(pca_scores, pca_var, x = 3, y = 4, pca_colour = indiv_colours,
+		legend = unique(populations), legend_colour = pop_colours, method = method)
 invisible(dev.off())
 
 
 # To interact with points and labels for exploring use plotly
+# Note: change PC1 to V1 when using the covariance matrix
 if (interactive) {
 	suppressMessages(library("plotly"))
-	data <- as.data.frame(pca$scores)
+	data <- as.data.frame(pca_scores)
 	plot_ly(data = data, x = ~PC1, y = ~PC2,
 			text = rownames(data), size = 15,
-			color = genl$pop, colors = pop_colours)
+			color = populations, colors = pop_colours)
 	plot_ly(data = data, x = ~PC1, y = ~PC3,
 			text = rownames(data), size = 15,
-			color = genl$pop, colors = pop_colours)
+			color = populations, colors = pop_colours)
 	plot_ly(data = data, x = ~PC1, y = ~PC4,
 			text = rownames(data), size = 15,
-			color = genl$pop, colors = pop_colours)
+			color = populations, colors = pop_colours)
 	plot_ly(data = data, x = ~PC2, y = ~PC3,
 			text = rownames(data), size = 15,
-			color = genl$pop, colors = pop_colours)
+			color = populations, colors = pop_colours)
 	plot_ly(data = data, x = ~PC3, y = ~PC4,
 			text = rownames(data), size = 15,
-			color = genl$pop, colors = pop_colours)
+			color = populations, colors = pop_colours)
 }
