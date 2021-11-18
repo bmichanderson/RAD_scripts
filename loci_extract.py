6126d409481cc6a5be6d1498b3a16e785e8a1a01
bmichanderson <banderson2914@gmail.com>
2021-11-18 14:03:43 +0800

    added ability to exclude samples

--- loci_extract.py
+++ loci_extract.py
@@ -3,6 +3,7 @@
 ##########################
 # Author: B. Anderson
 # Date: Sep 2021
+# Modified: Nov 2021 (added a sample exclude option)
 # Description: extract loci from an ipyrad .loci file based on an input list
 ##########################
 
@@ -19,6 +20,7 @@ parser = argparse.ArgumentParser(description = 'A script to extract loci from an
 # add arguments to parse
 parser.add_argument('loci_file', type = str, help = 'The .loci file to extract loci from')
 parser.add_argument('-l', type = str, dest = 'loci_list', help = 'A text file with a list of loci numbers, one per line (required)')
+parser.add_argument('-s', type = str, dest = 'samp_list', help = 'A text file with a list of sample names to exclude, one per line (optional)')
 
 
 # parse the command line
@@ -30,6 +32,8 @@ args = parser.parse_args()
 
 loci_file = args.loci_file
 loci_list = args.loci_list
+samp_list = args.samp_list
+
 
 if not loci_list:
 	parser.print_help(sys.stderr)
@@ -43,24 +47,43 @@ with open(loci_list, 'r') as loci:
         locus_nums.append(locus.rstrip())
 
 
+# read in the list of samples and assign to a list, if present
+if samp_list:
+	samp_names = []
+	with open(samp_list, 'r') as samples:
+		for sample in samples:
+			samp_names.append(sample.rstrip())
+	print('Will exclude ' + str(len(samp_names)) + ' samples from extracted loci')
+
+
 # extract the loci from the .loci file
 with open(loci_file, 'r') as locfile:
-    this_locus = []
-    found_loci = 0
-    processed_loci = 0
-    for line in locfile:
-        if line.startswith('//'):        # this indicates the final line of a locus
-            processed_loci = processed_loci + 1
-            locus_num = line.rstrip().split('|')[1]
-            if locus_num in locus_nums:
-                found_loci = found_loci + 1
-                with open('locus_' + str(locus_num) + '.fasta', 'w') as outfile:
-                    for entry in this_locus:
-                        outfile.write('>' + entry.rstrip().split()[0] + '\n' + entry.rstrip().split()[1] + '\n')
-            this_locus = []
-        else:
-            this_locus.append(line)
+	this_locus = []
+	found_loci = 0
+	processed_loci = 0
+	empty_loci = 0
+	for line in locfile:
+		if line.startswith('//'):        # this indicates the final line of a locus
+			processed_loci = processed_loci + 1
+			locus_num = line.rstrip().split('|')[1]
+			if locus_num in locus_nums:
+				found_loci = found_loci + 1
+				if len(this_locus) > 0:
+					with open('locus_' + str(locus_num) + '.fasta', 'w') as outfile:
+						for entry in this_locus:
+							outfile.write('>' + entry.rstrip().split()[0] + '\n' + entry.rstrip().split()[1] + '\n')
+				else:
+					empty_loci = empty_loci + 1
+			this_locus = []
+		else:
+			if samp_list:
+				if line.split()[0] not in samp_names:
+					this_locus.append(line)
+			else:
+				this_locus.append(line)
 
 
 # summarize the results
 print('Processed ' + str(processed_loci) + ' loci and extracted ' + str(found_loci) + ' loci of the targeted ' + str(len(locus_nums)))
+if empty_loci > 0:
+	print('Did not export ' + str(empty_loci) + ' loci that only included excluded taxa')
