9bf3603ead3647c9245dbb4dd34cede52cab6041
bmichanderson <banderson2914@gmail.com>
2021-12-17 19:09:41 +0800

    added a boxplot and adjusted the plotting

--- missing_plot.R
+++ missing_plot.R
@@ -21,7 +21,16 @@ myvcf <- read.vcfR(vcf_file, verbose = FALSE)
 gt <- extract.gt(myvcf, element = "GT")
 missing <- apply(gt, MARGIN = 2, function(x) { sum(is.na(x)) })
 missing <- 100 * missing / nrow(myvcf)
-pdf("missing.pdf", width = 34, height = 11)
+pdf("missing.pdf", width = 40, height = 20)
 par(mar = c(20, 4, 4, 2))
-barplot(missing, las = 2)
+par(fig = c(0, 0.9, 0, 1))
+barplot(missing, las = 2, cex.axis = 2, ylim = c(0, max(missing) + 0.1 * max(missing)))
+par(fig = c(0.85, 1, 0, 1), new = TRUE)
+boxplot(missing, axes = FALSE, ylim = c(0, max(missing) + 0.1 * max(missing)))
+mtext("Percent missing data per sample", side = 3,
+	outer = TRUE, line = -4, cex = 4)
 invisible(dev.off())
+
+
+# summarise
+summary(missing)
