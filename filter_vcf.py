2c4009116a3985bed7b5bcefff1cc437e3c23b50
bmichanderson <banderson2914@gmail.com>
2022-04-07 19:43:45 +0800

    added more pop filter options

--- filter_vcf.py
+++ filter_vcf.py
@@ -22,8 +22,10 @@ parser = argparse.ArgumentParser(description = 'A script to filter a VCF file')
 parser.add_argument('vcf_file', type = str, help = 'The VCF file to filter')
 parser.add_argument('-o', type = str, dest = 'outpre', help = 'Output file prefix [default \"output\"]')
 parser.add_argument('-s', type = str, dest = 'samps', help = 'A file with sample names to be dropped, one per line')
-parser.add_argument('-p', type = str, dest = 'pops', help = 'A file with samples and population identifiers, one per line and tab separated')
+parser.add_argument('-p', type = str, dest = 'pops', help = 'A file with samples and population identifiers (for pop filtering), one per line and tab separated')
 parser.add_argument('--minperpop', type = int, dest = 'minperpop', help = 'The minimum number of samples per population that must have a locus to keep it')
+parser.add_argument('--minpops', type = int, dest = 'minpops', help = 'The minimum number of populations that must have a locus to keep it')
+parser.add_argument('--inpop', type = str, dest = 'inpop', help = 'A file with a list of populations that must have a locus to keep it, one per line')
 parser.add_argument('--bial', type = str, dest = 'bial', help = 'Whether a locus must be biallelic to be kept (\"yes\" or \"no\" [default])')
 parser.add_argument('--mincov', type = float, dest = 'mincov', help = 'The minimum cover as a proportion of samples a locus must be found in to be kept')
 parser.add_argument('--mac', type = int, dest = 'mac', help = 'The minimum minor allele count (how many times the minor allele must be called to keep the locus)')
@@ -46,6 +48,8 @@ out_pre = args.outpre
 sample_file = args.samps
 pops_file = args.pops
 minperpop = args.minperpop
+minpops = args.minpops
+inpop_file = args.inpop
 bial = args.bial
 mincov = args.mincov
 minmac = args.mac
@@ -84,6 +88,14 @@ if pops_file:
 				pop_dict[pop] = [sampleID]
 			else:
 				pop_dict[pop].append(sampleID)
+
+
+# parse the inpop file, if present
+if inpop_file:
+	req_pops = []
+	with open(inpop_file, 'r') as input:
+		for pop in input:
+			req_pops.append(pop.rstrip())
 
 
 # parse and filter the VCF
@@ -142,6 +154,15 @@ with open(vcf_file, 'r') as vcf, open(out_pre + '.vcf', 'w') as outfile:
 			# calculate stats depending on what filters need to be applied
 			keep_locus = True
 			while keep_locus:
+				if inpop_file and pops_file:
+					for pop in req_pops:
+						pop_genos = [genotypes[ind] for ind, item in enumerate(pop_labels) if item == pop]
+						count = len([geno for geno in pop_genos if geno != ['.', '.']])
+						if count == 0:
+							keep_locus = False
+							break
+					if not keep_locus:
+						break
 				if minperpop and pops_file:
 					for pop in set(pop_labels):
 						pop_genos = [genotypes[ind] for ind, item in enumerate(pop_labels) if item == pop]
@@ -150,6 +171,18 @@ with open(vcf_file, 'r') as vcf, open(out_pre + '.vcf', 'w') as outfile:
 							keep_locus = False
 							break
 					if not keep_locus:
+						break
+				if minpops and pops_file:
+					pops_have = 0
+					for pop in set(pop_labels):
+						pop_genos = [genotypes[ind] for ind, item in enumerate(pop_labels) if item == pop]
+						count = len([geno for geno in pop_genos if geno != ['.', '.']])
+						if count > 0:
+							pops_have = pops_have + 1
+						if pops_have >= minpops:
+							break
+					if pops_have < minpops:
+						keep_locus = False
 						break
 				if mincov:
 					count_present = len([geno for geno in genotypes if geno != ['.', '.']])
