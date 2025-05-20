6a5a672afcf9e3a49e1022a417369b2cca022876
bmichanderson <banderson2914@gmail.com>
2025-05-20 22:10:55 +0800

    adjusted calculation error for number of comparisons by 2; added more comparative distances

--- SNAPP_divergence.R
+++ SNAPP_divergence.R
@@ -27,12 +27,14 @@ help <- function(help_message) {
 }
 
 
-# A function to calculate pairwise sequence divergence from 012 data
-# Note: this is calculated only on SNPs (if using that), so values are high
+# A function to calculate pairwise sequence divergence from 012 data (biallelic)
+# Note: this is calculated only on variable sites, SNPs (if using that), so values are high
 # It compares each row to every other row, and sums absolute distances
-# For example, if the SNP is "1", then "0", "1", and "2" = distances of 1, 0, and 1
+# Genotypes are: 0 = 00, 1 = 01, 2 = 11
+# Distances are between phased alleles, so there are 2 comparisons per site
+# For example, if sample1 has "1", then sample2 having "0", "1", and "2" = distances of 0+1, 0+0, and 1+0
 # Missing data ("?") is ignored (sites dropped for that comparison)
-# It then calculates substitutions per site by dividing by number of comparisons
+# It then calculates substitutions per site by dividing by number of comparisons (2 per site)
 # The argument is a matrix with samples as rows and SNPs as columns
 # Returns a (lower tri) matrix with substitutions per site between samples (rows and columns)
 sequence_diversity <- function(snp_mat) {
@@ -47,16 +49,16 @@ sequence_diversity <- function(snp_mat) {
 			missing <- append(missing, (ncol(snp_mat) - sum(not_miss)) / ncol(snp_mat))
 			comps <- snp_mat[c(row, comp_row), not_miss]
 			distance <- sum(abs(as.numeric(comps[1, ]) - as.numeric(comps[2, ])))
-			out_mat[comp_row, row] <-  distance / ncol(comps)
+			out_mat[comp_row, row] <-  distance / (2 * ncol(comps))
 		}
 		index <- index + 1
 	}
-	cat(paste0("Average missing comparisons proportion: ", round(mean(missing, na.rm = TRUE), 2), "\n"))
+	cat(paste0("Average missing comparisons proportion: ", round(mean(missing, na.rm = TRUE), 3), "\n"))
 	out_mat
 }
 
 
-# a function to calculate lambda (used for SNAPP)
+# a function to calculate lambda (used for SNAPP/ER)
 # Lambda is a parameter for the Yule Prior for the species tree and branch lengths
 # This equation is from "A Rough Guide to SNAPP"
 # lambda = 1/h * (sum(k=1 to n-1) k/(n*(n - k)))
@@ -118,10 +120,12 @@ subs_mat <- sequence_diversity(mymat)
 
 
 # report possible lambda values for different numbers of species (2–15)
-# need maximum divergence divided by two (height of the tree)
-height <- round(max(subs_mat, na.rm = TRUE) / 2, 2)
-cat(paste0("Tree height: ", height, " substitutions per site\n"))
-cat("Lambda (parameter for Yule Prior for species tree) for numbers of species:\n")
+# uses maximum divergence divided by two (height of the tree)
+height <- round(max(subs_mat, na.rm = TRUE) / 2, 4)
+cat(paste0("\nExpected tree height: ", height, " substitutions per site\n"))
+cat(paste0("Average divergence between all samples: ",
+	round(mean(subs_mat, na.rm = TRUE), 4)), "substitutions per site\n")
+cat("\nLambda (parameter for Yule Prior for species tree) by number of species:\n")
 cat("Species\tLambda\n")
 for (num in seq(2, 15)) {
 	lambda <- round(get_lambda(height, num), 1)
@@ -129,13 +133,13 @@ for (num in seq(2, 15)) {
 }
 
 
-# report average divergence within each population
+# report average divergences within populations
 cat("\nAverage substitutions per site between members of a population:\n")
 pops_to_subset <- unique(pops_table$pop[pops_table$sample %in% rownames(mymat)])
 
 result_df <- as.data.frame(matrix(nrow = length(pops_to_subset), ncol = 1))
 rownames(result_df) <- pops_to_subset
-colnames(result_df) <- c("Average_subs")
+colnames(result_df) <- c("Avg_intra_subs")
 
 for (pop in pops_to_subset) {
 	samples <- pops_table$sample[pops_table$pop == pop]
@@ -143,4 +147,28 @@ for (pop in pops_to_subset) {
 	result_df[pop, ] <- round(mean(comp_mat, na.rm = TRUE), 3)
 }
 result_df
-summary(result_df)
+cat(paste0("Min:\t", min(result_df, na.rm = TRUE), "\n"))
+cat(paste0("Avg:\t", round(mean(result_df[, 1], na.rm = TRUE), 3), "\n"))
+cat(paste0("Max:\t", max(result_df, na.rm = TRUE), "\n"))
+
+
+# report a distance matrix between populations and average between pop distances
+cat("\nAverage substitutions per site between populations:\n")
+result_mat <- matrix(NA, nrow = length(pops_to_subset), ncol = length(pops_to_subset))
+rownames(result_mat) <- pops_to_subset
+colnames(result_mat) <- pops_to_subset
+index <- 2
+for (pop_num in seq_len(length(pops_to_subset) - 1)) {
+	for (pop2_num in seq(index, length(pops_to_subset))) {
+		samples1 <- pops_table$sample[pops_table$pop == pops_to_subset[pop_num]]
+		samples2 <- pops_table$sample[pops_table$pop == pops_to_subset[pop2_num]]
+		comp_mat1 <- subs_mat[samples1, samples2]
+		comp_mat2 <- subs_mat[samples2, samples1]
+		result_mat[pops_to_subset[pop2_num], pops_to_subset[pop_num]] <- round(mean(c(comp_mat1, comp_mat2), na.rm = TRUE), 3)
+	}
+	index <- index + 1
+}
+result_mat
+cat(paste0("Min:\t", min(result_mat, na.rm = TRUE), "\n"))
+cat(paste0("Avg:\t", round(mean(result_mat, na.rm = TRUE), 3), "\n"))
+cat(paste0("Max:\t", max(result_mat, na.rm = TRUE), "\n"))
