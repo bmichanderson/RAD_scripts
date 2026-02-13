19f25725454d12feed177c8025f63fac4b93bfc2
bmichanderson <banderson2914@gmail.com>
2026-02-13 18:34:12 +0800

    adjusted figure size and added text output for missing_plot.R

--- missing_plot.R
+++ missing_plot.R
@@ -1,7 +1,8 @@
 
 ##########
-# Author: Ben Anderson
+# Author: B.M. Anderson
 # Date: Nov 2021
+# Modified: Feb 2026 (adjusted figure size and added output to text file)
 # Description: assess/plot the amount of missing data per sample in a VCF (arg1)
 ##########
 
@@ -15,13 +16,18 @@ if (length(args) == 0) {
 }
 
 
-# load the library, analyse the VCF, and plot missing data per sample
+# load the library and analyse the VCF
 suppressMessages(library(vcfR))
 myvcf <- read.vcfR(vcf_file, verbose = FALSE)
 gt <- extract.gt(myvcf, element = "GT")
 missing <- apply(gt, MARGIN = 2, function(x) { sum(is.na(x)) })
 missing <- 100 * missing / nrow(myvcf)
-pdf("missing.pdf", width = 40, height = 20)
+
+# set width relative to samples
+mywidth <- max(40, length(missing) / 5)
+
+# create PDF
+pdf("missing.pdf", width = mywidth, height = 20)
 par(mar = c(20, 4, 4, 2))
 par(fig = c(0, 0.9, 0, 1))
 barplot(missing, las = 2, cex.axis = 2, ylim = c(0, max(missing) + 0.1 * max(missing)))
@@ -31,6 +37,10 @@ mtext("Percent missing data per sample", side = 3,
 	outer = TRUE, line = -4, cex = 4)
 invisible(dev.off())
 
+# output the data to text file
+connect <- file("missing.txt", open = "w")
+write.table(missing, file = connect, col.names = FALSE, quote = FALSE)
+close(connect)
 
 # summarise
 summary(missing)
