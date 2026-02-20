731629b5860b46f96eab586d2150a69ef586e95a
bmichanderson <banderson2914@gmail.com>
2026-02-20 19:33:49 +0800

    cleaned up, removed warnings, updated call to groupby in bestK_Evanno.py

--- bestK_Evanno.py
+++ bestK_Evanno.py
@@ -1,10 +1,10 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 ##########################
-# Author: B. Anderson
+# Author: B.M. Anderson
 # Date: Oct 2021
-# Modified: Aug 2022 (changed font and output file format)
-# Description: compute and visualize likelihood values from multiple structure-like runs to assess best K
+# Modified: Aug 2022 (changed font and output file format); Feb 2026 (cleaned up and removed warnings)
+# Description: compute and visualize likelihood values from multiple Structure-like runs to assess best K
 ##########################
 
 
@@ -21,18 +21,13 @@ import matplotlib.pyplot as plt
 matplotlib.rcParams['font.family'] = 'Arial'
 
 
-# to avoid warnings when creating ragged arrays in np
-# (e.g. when the number of reps per K is different)
-np.warnings.filterwarnings('ignore', category = np.VisibleDeprecationWarning)
-
-
 # instantiate the parser
 parser = argparse.ArgumentParser(description = 'A script to compute and visualize likelihoods from Structure-like runs for assessing best K')
 
 
 # add arguments to parse
-parser.add_argument('-l', type = str, dest = 'like_file', help = 'The likelihoods file for the K reps (multiple lines per K) in tab delimited form:' + 
-																	'K_value likelihood')
+parser.add_argument('-l', type = str, dest = 'like_file', help = 'The likelihoods file for the K reps (multiple lines per K)' +
+	'in tab delimited form (first two columns): <K_value>	<likelihood>')
 parser.add_argument('-o', type = str, dest = 'output', help = 'Name of the output SVG without extension [default \"output\"]')
 
 
@@ -54,9 +49,7 @@ if not out_pre:
 
 # load the likelihoods file as a dataframe
 like_df = pd.read_csv(like_file, sep = '\t', header = None)
-like_df.rename(columns = {	0: 'K',
-							1: 'Likelihood'},
-							inplace = True)
+like_df.rename(columns = {	0: 'K', 1: 'Likelihood'}, inplace = True)
 
 
 # deltaK
@@ -67,7 +60,7 @@ like_df.rename(columns = {	0: 'K',
 # dK	= mean [|L(K + 1) - 2 L(K) + L(K - 1)|] / std [L(K)]
 
 # compute mean and standard deviation, and values needed for deltaK
-like_comps = like_df.groupby('K').agg([np.mean, np.std])
+like_comps = like_df.groupby('K').agg(['mean', 'std'])
 like_comps = like_comps['Likelihood']
 like_comps.loc[:, 'Lprime'] = like_comps['mean'] - like_comps['mean'].shift(1)
 like_comps.loc[:, 'absLdprime'] = np.abs(like_comps['Lprime'].shift(-1) - like_comps['Lprime'])
@@ -79,19 +72,18 @@ fig, (ax1, ax2) = plt.subplots(2, 1, sharex = True, figsize = (7, 9))
 plt.subplots_adjust(hspace = 0.2)
 plt.rcParams['font.size'] = 20
 like_comps.plot(y = 'mean', marker = '.', markersize = 12,
-					title = 'Log Prob of Data L(K)', legend = False,
-					linewidth = 0.75, color = 'steelblue', ax = ax1)
+	title = 'Log Prob of Data L(K)', legend = False,
+	linewidth = 0.75, color = 'steelblue', ax = ax1)
 ax1.grid()
 ax1.errorbar(x = like_comps.index, y = like_comps['mean'], yerr = like_comps['std'],
-			fmt = 'none', color = 'black', capsize = 4, elinewidth = 1, zorder = 3)
+	fmt = 'none', color = 'black', capsize = 4, elinewidth = 1, zorder = 3)
 ax1.get_figure().suptitle('')
 ax1.set_title('Log Prob of Data L(K)')
 ax1.ticklabel_format(style = 'sci', axis = 'y', scilimits = (0, 0))
 ax1.yaxis.get_offset_text().set_fontsize(18)
 ax1.yaxis.set_tick_params(labelsize = 18)
 like_comps.plot(y = 'deltaK', marker = '.', markersize = 12,
-					title = '\u0394K', legend = False,
-					color = 'steelblue', ax = ax2)
+	title = '\u0394K', legend = False, color = 'steelblue', ax = ax2)
 ax2.set_xlabel('K', fontsize = 22)
 ax2.set_xticks(like_comps.index)
 ax2.xaxis.set_tick_params(labelsize = 18)
